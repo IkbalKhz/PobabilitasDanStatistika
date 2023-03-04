a7cf4fde9dd4b9a8fee28772fdb6b124a4a3402b
IkbalKhz <100201974+IkbalKhz@users.noreply.github.com>
2023-03-04 21:50:13 +0800

    Add files via upload

---
+++ modus.py
@@ -0,0 +1,6 @@
+data = [0, 1, 2, 2, 4, 5]
+frekuensi = {}
+for nilai in data:
+    frekuensi[nilai] = frekuensi.get(nilai, 0) + 1
+modus = max(frekuensi, key=frekuensi.get)
+print("Modus dari himpunan data adalah:", modus)
